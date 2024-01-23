144396e4dd861161bba8e576f83d61f8fb34da9e
Xico26 <xicomaia2610@gmail.com>
2024-01-23 14:55:03 +0000

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        colors: {
+          'main-bg': 'hsl(233, 47%, 7%)',
+          'card-bg': 'hsl(244, 38%, 16%)',
+          'accent': 'hsl(277, 64%, 61%)',
+          'white-1': 'hsla(0, 0%, 100%, 0.75)',
+          'white-2': 'hsla(0, 0%, 100%, 0.6)'
+        }
+    }
+  }
